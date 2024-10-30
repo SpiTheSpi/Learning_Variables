e09321378b321e043cc655a1bd6e8bed1204704f
SpiTheSpi <spithespi@gmail.com>
2024-10-30 18:05:08 -0500

    Learning Variables.
    This is just the "free-style" or "free-form" coding I did for after I was done working on the coding tutorial video I was watching from the YouTube link below. Let me know what you guys think, or if there was anything else I could've done better!
    
    https://youtu.be/SgchdVZyzZ0?si=oHO671AM5uiAkGDx

---
+++ CwM - Variables.py
@@ -0,0 +1,27 @@
+'''
+This is going to be the first program on my Desktop
+in regards to running python programs.
+
+'''
+
+C_K = 'Caesar King| Hello Spi!'
+C_K_2 = 'Caesar King| Can you get me some groceries? I have the list for you right here.'
+print(C_K)
+print(C_K_2)
+# Blah Blah Blah Blah, we get it...
+
+Caesars_Grocerys = [
+
+'Apples',
+'Bread',
+'Cilantro'
+
+]
+
+print(Caesars_Grocerys[0])
+print(Caesars_Grocerys[1])
+print(Caesars_Grocerys[2])
+
+print("Caesar King| Thank you for getting the groceries for us, Spi!")
+print("Just before you head out of the door, ")
+print("Caesar, leaves a kiss on your cheek then sees you out the door.")
